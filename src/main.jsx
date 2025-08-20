01e213733fbf17cc668b2cb2163e35e0716fb153
2silver0711 <peu20816@gmail.com>
2025-08-20 15:20:41 +0900

    오늘의 투두리스트 연습

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './css/index.css';
-// import App from './App.jsx'; // 기본
-import App from './App.jsx'; // Ant design 반영, 구글아이콘, 검색기능 구현
+// import App from './App2.jsx'; // 기본
+import App from './App2.jsx'; // Ant design 반영, 구글아이콘, 검색기능 구현
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
